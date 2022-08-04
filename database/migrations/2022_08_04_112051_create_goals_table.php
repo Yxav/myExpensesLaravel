ceda5e5d4caed9c6cce7732f89681f37b8a48b46
Yuri Xavier <yuri_dsx_@hotmail.com>
2022-08-04 09:12:27 -0300

    migrations

---
+++ database/migrations/2022_08_04_112051_create_goals_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGoalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('goals', function (Blueprint $table) {
+            $table->id();
+            $table->string('short_name');
+            $table->decimal('amount', $precision = 8, $scale = 2);
+            $table->decimal('actual_balance', $precision = 8, $scale = 2);
+            $table->date('date_operation');
+            $table->timestamps();
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')
+                  ->references('id')
+                  ->on('users')
+                  ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('goals');
+    }
+}
