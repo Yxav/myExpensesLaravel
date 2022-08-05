71e89fe6d5350d8f3af23fb538738cb1cb8a770d
Yuri Xavier <yuri_dsx_@hotmail.com>
2022-08-05 17:34:07 -0300

    add relationship with: Incomes, Expenses and Goals to User

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,17 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function incomes()
+    {
+        return $this->hasMany(Income::class);
+    }
+    public function expenses()
+    {
+        return $this->hasMany(Expense::class);
+    }
+    public function goals()
+    {
+        return $this->hasMany(Goal::class);
+    }
 }
