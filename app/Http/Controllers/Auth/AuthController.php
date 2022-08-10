f5094317130f6738ca7eb58c3dde0a5e2f01c822
Yuri Xavier <yuri_dsx_@hotmail.com>
2022-08-10 09:40:34 -0300

    fix controller: remove redirect from controller

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -60,8 +60,7 @@ class AuthController extends Controller
         $credentials = $request->only('email', 'password');
 
         if (Auth::attempt($credentials)) {
-            return redirect()->intended('/')
-                    ->withSuccess('You have Successfully loggedin');
+            return response(200);
         }
         return redirect("login")->withSuccess('Oppes! You have entered invalid credentials');
     }
